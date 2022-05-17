1566ee7f3cebf9387e7c68fb4f2d75279e2b7344
Jesuan Rosin <jesuan34@gmail.com>
2022-05-16 22:23:41 -0300

    estilos del NavBar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './componentes/NavBar/NavBar';
@@ -8,18 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <NavBar />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavBar className="App-NavBar" />
       </header>
     </div>
   );
