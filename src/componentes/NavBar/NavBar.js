081fef1ef304ff924a6028287b7f6bbfb1bef9e0
Jesuan Rosin <jesuan34@gmail.com>
2022-05-16 21:54:10 -0300

    NavBar

---
+++ src/componentes/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return(
+        <nav>
+            <h1>Del Valle</h1>
+            <div>
+                <button>Macbook</button>
+                <button>Iphone</button>
+                <button>Accesorios</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
